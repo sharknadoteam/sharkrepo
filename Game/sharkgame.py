4be610ae15424695032166bd7285e26acc99c9ea
pierreh <pierreh@photonengr.com>
2017-10-17 11:27:03 -0700

    First commit of the initial Sharknado 4 files. Off to a great start!

---
+++ Game/sharkgame.py
@@ -0,0 +1,8 @@
+print("Welcome to Sharknado - The Game!\n\n")
+
+user_name = input("Please enter you character's name: ")
+
+print("Great! Welcome, " + user_name + "!")
+
+ready = input("Are you ready to play? (y/n)")
+
