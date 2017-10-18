98414e9ef2ef150e417e88ef16e83283a81547fb
pierreh <pierreh@photonengr.com>
2017-10-18 12:19:08 -0700

    Added an if/else block

--- Game/sharkgame.py
+++ Game/sharkgame.py
@@ -2,7 +2,12 @@ print("Welcome to Sharknado - The Game!\n\n")
 
 user_name = input("Please enter you character's name: ")
 
-print("Great! Welcome, " + user_name + "!")
+print("Great! Welcome!")
 
 ready = input("Are you ready to play? (y/n)")
 
+if (ready == 'y'):
+	print("Then let's start!")
+else:
+	print("Ok, bye!")
+	
